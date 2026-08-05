92f7c96d4a6519614d6fe6a6ffce4a98a3b56da8
TechTinker5 <upal00007@gmail.com>
2026-08-05 23:52:23 +0530

    updatevite

--- client/src/utils/api.js
+++ client/src/utils/api.js
@@ -1,9 +1,9 @@
 import axios from 'axios';
 
 //DEPLOY:
-// const api = axios.create({
-//     baseURL: import.meta.env.VITE_API_URL || '/api',
-// });
+const api = axios.create({
+    baseURL: import.meta.env.VITE_API_URL || '/api',
+});
 
 const api = axios.create({
     baseURL: 'http://localhost:5000/api' 
